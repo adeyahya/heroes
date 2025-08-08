c67e3ea7be32dc888822a553c631bc4aed3cf6c8
Ade Yahya <adeyahyaprasetyo@gmail.com>
2025-08-09 05:48:17 +0700

    feat: optim

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,20 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import playformCompress from '@playform/compress';
+
+import swup from '@swup/astro';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://heroes.inklusif.id',
-  integrations: [mdx(), sitemap(), tailwindcss()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwindcss(),
+    playformCompress(),
+    swup(),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Pahlawan Nasional";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Bersatu Berdaulat Rakyat Sejahtera Indonesia Maju";

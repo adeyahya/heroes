ef4360ce8d5c89292f955d60e753e71db3816888
Ade Yahya <adeyahyaprasetyo@gmail.com>
2025-08-09 03:13:31 +0700

    feat: adjust config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://heroes.inklusif.id',
   integrations: [mdx(), sitemap(), tailwindcss()],
 
   vite: {

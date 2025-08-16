15b4608505de734893968649688c5c63eec317ed
Ade Yahya <adeyahyaprasetyo@gmail.com>
2025-08-16 16:40:26 +0700

    feat: remove db

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-import db from '@astrojs/db';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://heroes.inklusif.id',
@@ -18,7 +16,6 @@ export default defineConfig({
     sitemap(),
     tailwindcss(),
     react(),
-    db(),
   ],
 
   vite: {
